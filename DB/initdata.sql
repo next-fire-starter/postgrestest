abc2d5650bb5968583b9f971548fb3810f29aae9
next-fire-starter <next.fire.starter@gmail.com>
2021-04-30 13:33:05 +0900

    Update initdata.sql

--- DB/initdata.sql
+++ DB/initdata.sql
@@ -1,5 +1,10 @@
---初期データ
+--初期データ メモ
 INSERT INTO techsc.memo (title, note) VALUES ('買い物', '歯ブラシと洗濯洗剤');
 INSERT INTO techsc.memo (title, note) VALUES ('打合せ予定', 'パワポ資料の用意');
 INSERT INTO techsc.memo (title, note) VALUES ('週末の予定1', '洗車');
 INSERT INTO techsc.memo (title, note) VALUES ('週末の予定2', 'クリーニング');
+
+--初期データ 従業員
+INSERT INTO techsc.employee(name, tel) VALUES('hogehoge', '000-0000-0000');
+INSERT INTO techsc.employee(name, tel) VALUES('piyopiyo', '111-1111-1111');
+INSERT INTO techsc.employee(name, tel) VALUES('fugafuga', '222-2222-2222');
