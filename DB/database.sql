28fff88cb0a289a9834d581269489a160410ce45
next-fire-starter <next.fire.starter@gmail.com>
2021-04-30 11:39:19 +0900

    Update database.sql

--- DB/database.sql
+++ DB/database.sql
@@ -1,7 +1,7 @@
 -- データベースの作成
-DROP DATABASE if EXISTS techdb;
+DROP DATABASE IF EXISTS techdb;
 CREATE DATABASE techdb OWNER gitpod;
 
 -- スキーマの作成
-DROP SCHEMA if EXISTS techsc;
+DROP SCHEMA IF EXISTS techsc;
 CREATE SCHEMA techsc AUTHORIZATION gitpod;
