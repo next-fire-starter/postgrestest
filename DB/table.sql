820f5e9f09bc9142578a9f8d31b69654e0ae3d35
next-fire-starter <next.fire.starter@gmail.com>
2021-04-30 13:32:04 +0900

    Update table.sql

--- DB/table.sql
+++ DB/table.sql
@@ -1,8 +1,19 @@
 -- テーブル定義
 
 -- メモ
-CREATE TABLE techsc.memo ( 
+
+DROP TABLE IF EXISTS techsc.memo;
+CREATE TABLE IF NOT EXISTS techsc.memo ( 
   title VARCHAR(20) NOT NULL, 
   note TEXT NOT NULL, 
   PRIMARY KEY (title)
 ) WITHOUT OIDS;
+
+-- 従業員
+DROP TABLE IF EXISTS techsc.employee;
+CREATE TABLE IF NOT EXISTS employee (
+    id serial,
+    name text NOT NULL unique,
+    tel text,
+    primary key(id)
+);
